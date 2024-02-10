e4ff1efbc2a93e736723c97565f6a94285ee3f53
Andrei Dziaineka <dziaineka@gmail.com>
2024-02-10 23:04:34 +0100

    add parse_file function

--- src/dotenv_config_parser.erl
+++ src/dotenv_config_parser.erl
@@ -14,4 +14,38 @@
     | convert_function().
 
 -export_type([parser/0, convert_function/0]).
--export([parse_file/1, parse_config/1]).
+-export([parse_file/1, parse_config/2]).
+
+-spec parse_file(file:name_all()) -> {ok, [{binary(), binary()}]} | {error, binary()}.
+parse_file(FileName) ->
+    case file:read_file(FileName) of
+        {ok, FileContent} ->
+            parse_file_content(FileContent);
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+-spec parse_file_content(binary()) -> {ok, [{binary(), binary()}]} | {error, binary()}.
+parse_file_content(FileContent) ->
+    Lines = binary:split(FileContent, <<"\n">>, [global, trim_all]),
+    lists:foldl(
+        fun(Line, ParsedLines) ->
+            case parse_line(Line) of
+                {ok, ParsedLine} ->
+                    [ParsedLine | ParsedLines];
+                error ->
+                    exit(<<"Can't parse this line from doteenv config: ", Line/binary>>)
+            end
+        end,
+        [],
+        Lines
+    ).
+
+-spec parse_line(binary()) -> {ok, {binary(), binary()}} | error.
+parse_line(Line) ->
+    case binary:split(Line, <<"=">>, [trim_all]) of
+        [Key, Value] ->
+            {ok, {string:trim(Key), string:trim(Value)}};
+        _ ->
+            error
+    end.
